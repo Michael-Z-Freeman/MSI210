4effd7f10e0111797e2ace00f53876b9de3a9962
Michael Z Freeman <michael@michaelzfreeman.org>
2024-03-03 16:58:46 +0000

    Add files via upload

---
+++ website.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function( $ ){
+	// Adds link to top page to "Michael Z Freeman" 3D logo thus creating a "home" button.
+	$( "#menu-top" ).prepend( "<a href=\"https://music.michaelzfreeman.org/\"><img id=\"mzf-logo\" src=\"https://music.michaelzfreeman.org/wp-content/uploads/2024/01/cropped-mzf-3d-text-2-crop.png\" width=\"350\" height=\"107\" /></a>" );
+});
